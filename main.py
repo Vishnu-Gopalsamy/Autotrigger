7cf6fe9b9939814bf5a134d91040cf0dbd2bdfb6
Vishnu Gopalsamy <155133254+Vishnu-Gopalsamy@users.noreply.github.com>
2026-04-20 08:46:31 +0530

    Set up Flask app for CI pipeline success message

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-print("Hello World")
-print("Changed now")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>CI Pipeline Working Successfully 🚀</h1>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
