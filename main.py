8cd3bbc31b1c4557514c4d7a19cf7a201de01f7d
Vishnu Gopalsamy <155133254+Vishnu-Gopalsamy@users.noreply.github.com>
2026-04-20 11:04:21 +0530

    Add print statements to main.py

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return "<h1>CI Pipeline Working Successfully 🚀</h1>"
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+print("Hello world")
+print("Check")
